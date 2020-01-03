beec7d157df108b32b5f97b236243143bdcb26fc
Marcelo Hervas Granados <marcelohervas@gmail.com>
2020-01-03 12:08:50 +0100

    RestServer - login de usuario

---
+++ server/routes/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+//rutas
+app.use(require('./usuario'));
+app.use(require('./login'));
+
+module.exports = app;
--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json. Middleware
 app.use(bodyParser.json());
-app.use(require('./routes/routes'));
+app.use(require('./routes/index'));
 
 
 mongoose.connect(process.env.URLDB, {
